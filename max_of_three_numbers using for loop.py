c5dcd0e6d77e27f9d8cdce68b373e765bdfaa536
Bharath R <bharathr512@duck.com>
2025-05-25 02:39:45 +0530

    Commit

--- max_of_three_numbers using for loop.py
+++ max_of_three_numbers using for loop.py
@@ -1,1 +1,13 @@
-print("Hello")
+list=[]
+
+a = int(input("Enter how many numbers you want to check: "))
+for i in range(a):
+    num = int(input("Enter the number: "))
+    list.append(num)
+
+max = list[0]
+
+for i in list:
+    if i > max:
+        max = i
+print("Max number is: " + str(max))
